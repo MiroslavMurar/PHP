13d1daec78ae816b655e227eb419c8333556f49d
Miroslav Murar <mgr.miroslav.murar@gmail.com>
2019-06-19 23:39:13 +0200

    .

---
+++ W3School_DataTypes/PHP7_Advanced/PHP7_DateAndTime.php
@@ -0,0 +1,17 @@
+<?php
+//echo "Today is " . date("Y/m/d") . "<br>";
+//echo "Today is " . date("Y.m.d") . "<br>";
+//echo "Today is " . date("Y-m-d") . "<br>";
+//echo "Today is " . date("l");
+//
+//?>
+
+
+<!--&copy; 2010---><?php //echo date("Y");?>
+
+
+
+<?php
+date_default_timezone_set("America/New_York");
+echo "The time is " . date("h:i:sa");
+?>
