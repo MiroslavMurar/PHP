92b2539925ba85d8ea2017d6f9a621cc887ee03b
Miroslav Murar <mgr.miroslav.murar@gmail.com>
2019-06-20 08:47:21 +0200

    .

---
+++ W3School_DataTypes/PHP7_Advanced/PHP7_FileCreateWrite.php
@@ -0,0 +1,42 @@
+<?php
+//$myfile = fopen("testfile.txt", "w");
+
+//$myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
+//$txt = "John Doe\n";
+//fwrite($myfile, $txt);
+//$txt = "Jane Doe\n";
+//fwrite($myfile, $txt);
+//fclose($myfile);
+//
+
+
+$myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
+//$txt = "Mickey Mouse\n";
+//fwrite($myfile, $txt);
+//$txt = "Minnie Mouse\n";
+//fwrite($myfile, $txt);
+fclose($myfile);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
---
+++ W3School_DataTypes/PHP7_Advanced/PHP7_FileHandling.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: miro
+ * Date: 6/19/19
+ * Time: 11:39 PM
+ */
