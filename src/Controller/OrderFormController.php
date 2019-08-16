b5c9d98e8941d568ccd3b09cfe7f9b78a8c54707
Miroslav Murar <mgr.miroslav.murar@gmail.com>
2019-08-16 23:00:02 +0200

    Vsetko nakopirovane od Ludeka

--- src/Controller/OrderFormController.php
+++ src/Controller/OrderFormController.php
@@ -3,9 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\Order;
+use App\Entity\OrderProduct;
 use App\Entity\Product;
 use App\Form\OrderType;
 
+use App\Repository\BasketRepository;
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -16,6 +19,60 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class OrderFormController extends AbstractController
 {
+
+    /**
+     * @Route("/", name="order_form_index", methods="GET|POST")
+     */
+    public function index(
+        Request $request,
+        BasketRepository $basketRepository,
+        ProductRepository $productRepository
+    ): Response {
+        $em = $this->getDoctrine()->getManager();
+        $order = new Order;
+
+        $form = $this->createForm(OrderType::class, $order);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            foreach ($basketRepository->getBasket() as $orderProductData) {
+
+                $product = $productRepository->find($orderProductData[BasketRepository::KEY_PRODUCT_ID]);
+
+                if (!$product || ($product->getStock() - $orderProductData[BasketRepository::KEY_PRODUCT_AMOUNT]) < 0) {
+                    return $this->render('order_form/index.html.twig', [
+                        'order' => $order,
+                        'form' => $form->createView(),
+                        'remainingOnStock' => $product ? $product->getStock() : null
+                    ]);
+                }
+
+                $orderProduct = new OrderProduct();
+                $orderProduct->setName($orderProductData[BasketRepository::KEY_PRODUCT_NAME]);
+                $orderProduct->setUnitPrice($orderProductData[BasketRepository::KEY_PRODUCT_PRICE]);
+                $orderProduct->setAmount($orderProductData[BasketRepository::KEY_PRODUCT_AMOUNT]);
+                $em->persist($orderProduct);
+                $order->addProduct($orderProduct);
+
+                $product->setStock($product->getStock() - $orderProductData[BasketRepository::KEY_PRODUCT_AMOUNT]);
+            }
+
+            $order->updateTotalPrice();
+            $order->setCreated(new \DateTime);
+            $em->persist($order);
+            $em->flush();
+
+            return $this->redirectToRoute('order_form_confirm');
+        }
+
+        return $this->render('order_form/index.html.twig', [
+            'order' => $order,
+            'form' => $form->createView(),
+            'basket' => $basketRepository->getBasket()
+        ]);
+    }
+
+
 
     /**
      * @Route("/{id}/new", name="order_form", methods={"GET","POST"})
